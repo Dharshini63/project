ce8c7f09bd37da0d5d6046d7cee061392e239eef
Dharshini63 <dharshin200563@gmail.com>
2023-11-22 11:36:03 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,57 @@
+import './App.css';
+import AxiosGet from './day8/AxiosGet';
+
+export default function App() {
+  return (
+    <div className="App">
+      {/* <List2/> */}
+      {/* <Key/> */}
+      {/* <List/> */}
+      {/* <PrimarySearchAppBar/> */}
+      {/* <FormPropsTextFields/> */}
+      {/* <RatingSize/> */}
+      {/* <FormDialog/> */}
+      {/* <PositionedSnackbar/> */}
+      {/* <SimpleBackdrop/> */}
+      {/* <Day4cwhelp1/> */}
+      {/* <Day4cwhelp2/> */}
+      {/* <Day4cwhelp3/> */}
+      {/* <Day4cw/> */}
+      {/* <Hooksconcept/> */}
+      {/* <PropsFunc name="Imran" age="18" /> */}
+      {/* <MyComponent/> */}
+      {/* <Ques20/> */}
+      {/* <Ques32/> */}
+      {/* <Ques30/> */}
+      {/* <ErrorBoundary2>
+        <ErrorBoundary1 studentName="Harish"/>
+      </ErrorBoundary2> */}
+      {/* <TryCatch name='Aja'/> */}
+      {/* <Loginpage/> */}
+      {/* <HoverCouunter/> */}
+      {/* <Propvalidation name={12}/> */}
+      {/* <SpreadpassUnkonown title1='SKCET' type='password' placeholder='Enter Password' id='i'/> */}
+      {/* <Reference/> */}
+      {/* <BrowserRouter>
+      <ul>
+          <li>
+            <Link to="/Home">Home</Link>
+          </li>
+        </ul>
+        <ul>
+          <li>
+            <Link to="/About">About</Link>
+          </li>
+       </ul> */}
+       {/* <Navbar/> */}
+          {/* <Routes>
+            <Route path='/Home' element={<Home/>}></Route>
+            <Route path='/About' element={<About/>}></Route>
+          </Routes>
+      </BrowserRouter> */}
+      {/* <Day4PAH/> */}
+      <AxiosGet/>
+    </div>
+  );
+}
+
---
+++ src/day7/About.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div>This iss about page</div>
+  )
+}
++
